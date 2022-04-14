d438fa8f9861e556362a4cbcc79853d0d4e9f20a
khalidhersi <cipherpolfoundation@gmail.com>
2022-04-14 23:22:55 +0100

    game renamed to dodge

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,7 +9,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Paper Plane");
+        window.setTitle("Dodge");
 
         FramePanel framePanel = new FramePanel();
         window.add(framePanel);
